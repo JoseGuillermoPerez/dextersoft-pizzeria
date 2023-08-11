0a671a111d552e4ee894058cad9c692713168306
jguillermodex23@gmail.com <jguillermodex23@gmail.com>
2023-08-11 18:59:42 -0400

    Añadir estructura inicial del proyecto.

---
+++ src/main/java/com/dextersoft/pizzeria/DextersoftPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.dextersoft.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DextersoftPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DextersoftPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dextersoft/pizzeria/DextersoftPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dextersoft.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DextersoftPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
